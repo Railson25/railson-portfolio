376da8073deec7bf90aa07b374cdc2efb18fb941
Railson25 <railsonsantiago123@gmail.com>
2023-11-06 17:55:22 -0300

    <style>: use global variables

--- components/intro.tsx
+++ components/intro.tsx
@@ -78,7 +78,7 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />{" "}
         </Link>
         <a
-          className="group bg-white  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10"
+          className="group bg-white  px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer borderBlack"
           href="/Railson.pdf"
           download
         >
@@ -87,14 +87,14 @@ export default function Intro() {
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />{" "}
         </a>
         <a
-          className="bg-white p-4 flex items-center gap-2 text-gray-700 rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 transition cursor-pointer border border-black/10 hover:text-blue-800"
+          className="bg-white p-4 flex items-center gap-2 text-gray-700 rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 transition cursor-pointer borderBlack hover:text-blue-800"
           target="_blank"
           href="https://www.linkedin.com/in/railson-santiago-97991516b/"
         >
           <BsLinkedin />
         </a>
         <a
-          className="bg-white p-4 flex items-center gap-2 text-gray-700 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15]  hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          className="bg-white p-4 flex items-center gap-2 text-gray-700 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15]  hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack"
           target="_blank"
           href="https://github.com/Railson25"
         >
