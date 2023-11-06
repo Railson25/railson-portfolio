1be4d72b6f34e4aca5176d67e97e1c1bcae37399
Railson25 <railsonsantiago123@gmail.com>
2023-11-06 09:57:21 -0300

    <feat>: getting timeoslastclick to stopr scroll when users click

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import React, { createContext, useContext, useState } from "react";
-import { links } from "@/lib/data";
 
-type SectionName = (typeof links)[number]["name"];
+import type { SectionName } from "@/lib/types";
 
 type ActiveSectionContextType = {
   activeSection: SectionName;
   setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
+  timeOfLastClick: number;
 };
 
 export const ActiveSectionContext =
@@ -19,8 +20,17 @@ export default function ActiveSectionContextProvider({
   children: React.ReactNode;
 }) {
   const [activeSection, setActiveSection] = useState<SectionName>("Home");
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
+
   return (
-    <ActiveSectionContext.Provider value={{ activeSection, setActiveSection }}>
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
+      }}
+    >
       {children}
     </ActiveSectionContext.Provider>
   );
