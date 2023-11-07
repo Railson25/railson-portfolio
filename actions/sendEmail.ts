3b186c9b9e9f68da92a4de4051345b26a7935a47
Railson25 <railsonsantiago123@gmail.com>
2023-11-07 14:58:48 -0300

    <refactor>: send error data back to the client

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import { getErrorMessage, validateString } from "@/lib/utils";
-
+import ContactFormEmail from "@/email/contact-form-email";
 import { Resend } from "resend";
+import React from "react";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -21,17 +22,26 @@ export const sendEmail = async (FormData: FormData) => {
     };
   }
 
+  let data;
+
   try {
-    await resend.emails.send({
+    data = await resend.emails.send({
       from: "Contact form <onboarding@resend.dev>",
       to: "testesantiago654@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
-      text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: message as string,
+      }),
     });
   } catch (error: unknown) {
     return {
       error: getErrorMessage(error),
     };
   }
+
+  return {
+    data,
+  };
 };
